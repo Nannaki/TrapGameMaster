42655a3d56abbb8b4b1eb040beb35b62de91258e
987FacesOfWeb <julien.ramirez@gmail.com>
2022-03-31 23:50:00 +0200

    comment db.js on backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
+//Appel des requis
 const mongoose = require('mongoose');
 const colors = require('colors');
 
+//Connection à la BDD
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+
     }catch (error) {
         console.log(error);
         process.exit(1);
     }
 }
 
+//Export de la connection
 module.exports = connectDB;
